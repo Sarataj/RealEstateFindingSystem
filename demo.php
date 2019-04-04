51820db7f57c9eaba24f74f428fbca04a51cfe65
Sarataj Sultan <saratajsultan@gmail.com>
2019-04-04 18:34:38 +0600

    me tried different way
    me tried different way

---
+++ demo.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="utf-8">
+<title>Colorlib | Free Bootstrap Website Template</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+
+<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="assets/css/template.css" rel="stylesheet" media="screen">
+<script>if ( top !== self ) top.location.replace( self.location.href );// Hey, don't iframe my iframe!</script>
+
+<link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700,800,300" rel='stylesheet' type='text/css'>
+<!--[if lt IE 9]>
+		<script>/*@cc_on'abbr article aside audio canvas details figcaption figure footer header hgroup mark meter nav output progress section summary subline time video'.replace(/\w+/g,function(n){document.createElement(n)})@*/</script>
+	<![endif]-->
+</head>
+<body>
+
+<header class="switcher-bar clearfix">
+
+<div class="logo textual pull-left">
+<a href="https://colorlib.com/wp/templates/" title="Switcher">
+<img src="assets/img/logo.png" alt="uiCookies">
+</a>
+</div>
+
+<div class="product-switcher pull-left">
+<a href="#" title="Select a Product">
+Select a Product <span>+</span>
+</a>
+</div>
+
+<div class="remove-btn header-btn pull-right">
+<a href="#" title="Close this bar" class="icon-remove"></a>
+</div>
+
+<div class="purchase-btn header-btn pull-right">
+<a href="#" title="Download" class="icon-cloud-download"></a>
+</div>
+
+<div class="mobile-btn header-btn pull-right hidden-xs">
+<a href="#" title="Smartphone View" class="icon-mobile-phone"></a>
+</div>
+
+<div class="tablet-btn header-btn pull-right hidden-xs">
+<a href="#" title="Tablet View" class="icon-tablet"></a>
+</div>
+
+<div class="desktop-btn header-btn pull-right hidden-xs">
+<a href="#" title="Desktop View" class="icon-desktop"></a>
+</div>
+</header>
+
+<section class="switcher-body">
+<a href="#" title="Prev" class="icon-chevron-left products-prev"></a>
+<div class="products-wrapper">
+<div class="products-list clearfix">
+</div>
+</div>
+<a href="#" title="Next" class="icon-chevron-right products-next"></a>
+</section>
+
+<iframe class="product-iframe" frameborder="0" border="0"></iframe>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="assets/js/products.js"></script>
+<script src="assets/js/application.min.js"></script>
+</body>
+</html>
---
+++ try.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <style>
+         body {
+            background-image: url("b1.jpg");background-repeat: no-repeat; background-size: 100%;height: 800 px; background-attachment: fixed;
+			
+         }
+		 #container {
+    width: 100%;
+    max-width: 700px;
+    margin: 2em auto;
+}
+.cols {
+    -moz-column-count:5;
+    -moz-column-gap: 5%;
+    -moz-column-width: 50%;
+    -webkit-column-count:5;
+    -webkit-column-gap: 5%;
+    -webkit-column-width: 50%;
+    column-count: 5;
+    column-gap: 5%;
+    column-width: 50%;
+}
+.box {
+    margin-bottom: 20px;
+    height:100px;
+    background:#BFBFFF;
+}
+      </style>
+   </head>
+
+   <body>
+      <h1>Connect with Tutors</h1>
+   </body>
+   
+   <div id="container" class="cols">
+    <div class="box">Hello Moto</div>
+    <div class="box">img src="b1.jpg"</div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+    <div class="box"></div>
+  
+</div>
+</html>
