f1313279ba31e60ec63168353a5f81944880b710
Sarataj Sultan <saratajsultan@gmail.com>
2019-04-04 18:34:10 +0600

    contact
    contact  part

---
+++ contact.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+		<title>Contact Information</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	</head>
+	<body>
+					<header id="header">
+				<div class="inner">
+					<a href="index.php" class="logo">BirdNest Home</a>
+					<nav id="nav">
+						<!--<a href="index.html">Home</a>-->
+						<a class="w3-large" href="User_login.php">Login</a>
+						<a class="w3-large" href="User_Registration.php">Registration</a>
+						<a class="w3-large" href="contact.php">Contact Us</a>
+					</nav>
+				</div>
+			</header>
+		<section id="banner">
+			
+				
+				<h1 class="heading">Contact Us</h1>
+				<center><b><h2> Developer </h2></b></center>
+		<div class="log">
+			<table>
+				<tr>
+						Sarataj Sultan<br>
+						Student, 9th semester<br>
+						Daffodil International University<br>
+						Mail: saratajsultan@gmail.com<br>
+						Phone +8801774295054
+				</tr>
+				<br><br><br>
+				<tr>
+						Mizanur Rahman<br>
+						Student, 9th semester<br>
+						Daffodil International University<br>
+						Mail: saratajsultan@gmail.com<br>
+						Phone +8801774295054
+				</tr>
+			</table>
+		</div>
+
+			</section> 
+	
+		
+		<div class="footer">
+			<footer><p>Copyright &copy; Sarataj Sultan </p></footer>
+		</div>
+
+	</body>
+</html>
