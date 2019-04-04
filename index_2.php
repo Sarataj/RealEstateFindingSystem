37cb7a4e1a2b3636cc462e3369bcee5b4a8b629f
Sarataj Sultan <saratajsultan@gmail.com>
2019-04-04 18:32:40 +0600

    Dashboard or index
    Dashboard or index

---
+++ index_2.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html>
+	<head> 
+		<title>Real-Estate Finding System</title>
+		<link rel="stylesheet" href="style.css"> 
+	<?php include"navbar.php";?> 
+	</head>
+	<br>
+	<body class="back">
+	<div class="log">
+	<!--<marquee behavior="alternate"><img src="b1.jpg" width="800" height ="300"></marquee><br>
+	<marquee direction ="right"><img src="b0.jpg" width="800" height ="300"></marquee>
+	-->
+	
+       
+	</div>
+	<br><br><br>
+<div class="footer">
+			<footer><p>Copyright &copy; Sarataj Sultan </p></footer>
+		</div>
+</body>
+
+
+
+
+
+<div class="navbar-collapse  collapse">
+              <ul class="nav navbar-nav navbar-right">
+               <li class="active"><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+              </ul>
+            </div>
+
+</html>
+
+
+
