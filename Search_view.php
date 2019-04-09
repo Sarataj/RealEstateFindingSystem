c0d22378242df86d6cbd67b95ae3f94f5726da0c
Sarataj Sultan <saratajsultan@gmail.com>
2019-04-09 23:32:50 +0600

    user view
      user view

--- Search_view.php
+++ Search_view.php
@@ -1,24 +1,14 @@
 <!DOCTYPE HTML>
 
 <?php
-
 	include ("database.php");
 	session_start();
-	if(!isset($_SESSION["U_id"])){
-		
-		//echo"<script>window.open('User_login.php?mes=Access Denied...','_self');</script>";
+	if(!isset($_SESSION["U_id"]))
+	{
 		$_SESSION["U_id"] = null;
-		//$_SESSION["A_ID"] = null;
 	}
-	
 	$sql=mysqli_query($db,"SELECT U_name FROM user_info WHERE U_id={$_SESSION["U_id"]}");
-	//$sql_1=mysqli_fetch_array($sql);
-?>
-<?php
 	$sql_1 = mysqli_query($db,"SELECT * FROM request_to_sell_product where P_approval != 'need_approval'");
-	//$sql_1=mysqli_query($db,"SELECT * FROM request_to_sell_property where P_ref_name != = 'null'");// where P_ref_name != "null""
-	//$res=mysqli_fetch_array($sql_1);
-
 ?>
 <html>
 	<head>
@@ -34,34 +24,27 @@
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 	<body>
-
-			<header id="header">
-				<div class="inner">
-					<a href="index.php" class="logo">BirdNest Home</a>
-					<nav id="nav">
-					
-						<a class="w3-large" href="User_login.php">Login</a>
-						<a class="w3-large" href="User_Registration.php">Registration</a>
-						<a class="w3-large" href="contact.php">Contact Us</a>					
-					</nav><a><div  class ="search-form">
-					
-							<form action="Search_view.php" method="post">
-								<input type="text" name="search" placeholder="Name/Address">
-								<button type="submit" class="btn_2" value="save" name="submit">Find Now</button>
-							</form></div></a>
-							
-							
+		<header id="header">
+			<div class="inner">
+				<a href="index.php" class="logo">BirdNest Home</a>
+				<nav id="nav">			
+					<a class="w3-large" href="User_login.php">Login</a>
+					<a class="w3-large" href="User_Registration.php">Registration</a>
+					<a class="w3-large" href="contact.php">Contact Us</a>					
+				</nav><a><div  class ="search-form">
+				
+					<form action="Search_view.php" method="post">
+						<input type="text" name="search" placeholder="Name/Address">
+						<button type="submit" class="btn_2" value="save" name="submit">Find Now</button>
+					</form></div></a>						
 				</div>
 			</header>
-
 		<!-- Banner -->
 			<section id="banner">
 				<h1>Welcome to BirdNest</h1>
 				<p>an Online Property Finder</p>
 
-					</section>
-		
-
+			</section>
 		<!-- Two -->
 		<section id="two" class="wrapper style1 special">
 				<div class="inner">
@@ -90,27 +73,24 @@
 									
 									$sql_search=mysqli_query($db,"SELECT * FROM request_to_sell_product where P_location LIKE '%$search_item%' or P_owner_name LIKE '%$search_item%' ");
 										if($sql_search != null)
-										{
-																
-											while($res=mysqli_fetch_array($sql_search)){
-											?>
-											<div class="d-flex flex-row flex-wrap flex-4">
-												<div class="box person">
-													<div class="image square">
-														<!--<img src="properties/1.jpg" alt="Person 1" /> -->
-													</div>
-													<h3>    </h3>
-													<?php //$id = "<p>".$res['P_location']."</p>";?>
-													<?php $_SESSION["P_id"] = "$res[P_id]"; 
-													echo '<img height = "180" width = "180" src="data:P_pic_1;base64,'.$res['P_pic_1'].'">';?>
-													<?php echo "<p>".$res['P_type']."</p>";?> at <?php echo "<p>".$res['P_location']."</p>";?> for <?php echo "<p>".$res['P_for_sell_rent']."</p>";?>
-													<?php echo "<a href=\"Property_Details.php?P_id=$res[P_id]\">View Details</a></td>";		?>	
-													<!--echo "<td><a href=\"monitoring.php?P_id=$res[P_id]\">Monitor Request</a></td>";	-->					
-												</div>
-											</div>
-											
+										{?>					
+											<div class="display: flex" >
 											<?php
-											}
+											while($res=mysqli_fetch_array($sql_search)){
+												?>
+														<div class="box person" >
+															<div class="image square">
+																<center><?php $_SESSION["P_id"] = "$res[P_id]"; 
+																echo '<img height = "180" width = "180" src="data:P_pic_1;base64,'.$res['P_pic_1'].'">';?> </center>
+																<?php echo "<p>".$res['P_type']."</p>";?> at <?php echo "<p>".$res['P_location']."</p>";?> 
+																<?php echo "<p>".$res['P_for_sell_rent']."</p>";?>
+																<?php echo "<a href=\"Property_Details.php?P_id=$res[P_id]\">View Details</a></td>";?>		
+															</div>
+														</div>
+													<?php
+													}
+												?>
+											</div> <?php
 									}
 									
 									//$sql_search=mysqli_query($db,"SELECT * FROM request_to_sell_product where P_location LIKE '%$search_item%' or P_owner_name LIKE '%$search_item%' ");
